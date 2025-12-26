743b70a088f6d4e7edd5c8613f7d7d4172bd95af
Vrxshal <vrielan@gmail.com>
2025-12-26 12:24:29 +0800

    did some changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-// =============================================================
 //  FOMALHAUT TT6L
 //  CCP6124 SPACE BATTLE PROJECT (Trimester 2530)
-//  Member 4: YOU (Main.cpp, Operator Overloading, Memory Mgmt & Testing)
-//
-//  This file is the MAIN CONTROLLER that connects all modules.
-//  It will be compiled using:
-//      g++ FomalhautTT6L.cpp -o FomalhautTT6L
-// =============================================================
+//  Member 4: YOU (Main.cpp & Testing)
 
 #include <iostream>
 #include <vector>
@@ -15,13 +9,9 @@
 #include <ctime>
 using namespace std;
 
-// =============================
-//   Forward declarations
-//   (Actual class definitions implemented by Members 1 & 2)
-// =============================
-class Ship;               // Base abstract ship class (Member 1)
-class Weapon;             // Abstract weapon (Member 1)
-class CrewMember;         // Abstract crew member (Member 1)
+class Ship;               
+class Weapon;             
+class CrewMember;         
 
-vector<Ship*> loadShips(string file);      // Member 3 implements
-vector<CrewMember*> loadCrew(string file); // Member 3 implements
+vector<Ship*> loadShips(string file);      
+vector<CrewMember*> loadCrew(string file); 
